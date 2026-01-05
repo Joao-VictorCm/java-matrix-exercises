a7a6fc0c7c1357657b4b710eae428baae0b3136e
casti <joaovictor27cm@gmail.com>
2026-01-04 21:20:13 -0300

    Finalizado a logica do ex 01

--- src/exercises_01/Diagonal_negativos.java
+++ src/exercises_01/Diagonal_negativos.java
@@ -1,9 +1,38 @@
 package exercises_01;
 
+import java.util.Scanner;
+
 public class Diagonal_negativos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = 3;
+		
+		int count = 0;
+		int[][] mat = new int[n][n];
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				System.out.println("Elemento ["+i + "," + j+"]");
+				mat[i][j] = sc.nextInt();
+				if(mat[i][j] < 0) {
+					count++;
+				}
+			}
+		}
+		
+		
+		System.out.println("DIAGONAL PRINCIPAL:");
+		for(int i =0; i<n; i++) {
+			System.out.print(mat[i][i]+ " ");
+		}
+		
+		System.out.println();
+
+		System.out.println("QUANTIDADE DE NEGATIVOS: "+count);
+		
 
 	}
 
